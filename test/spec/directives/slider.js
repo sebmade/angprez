1ca32082a8b1cffff09b334aa0c8e22f4b7519dc
Sebastien Letelie <sebastien.letelie@gmail.com>
2012-12-14 23:14:46 +0100

    test pass

--- test/spec/directives/slider.js
+++ test/spec/directives/slider.js
@@ -9,7 +9,7 @@ describe('Directive: slider', function() {
 
   beforeEach(inject(function($rootScope, $compile, $httpBackend) {
     $httpBackend.when("GET", "/data/slides.json").respond([{title: "test", content: "slide0.html"}]);
-    $httpBackend.when("GET", "slide0.html").respond("<div></div>");
+    $httpBackend.when("GET", "slide0.html").respond('<div id="slide0">test</div>');
     element = angular.element('<slider slides="/data/slides.json"></slider>');
     element = $compile(element)($rootScope);
     $rootScope.$digest();
@@ -18,5 +18,6 @@ describe('Directive: slider', function() {
 
   it('should show title', function() {
     expect(element.find('div div img').attr("src")).toBe('images/AngularJS-Shield-small.png');
+    expect(element.find('#slide0').text()).toBe('test');
   });
 });
